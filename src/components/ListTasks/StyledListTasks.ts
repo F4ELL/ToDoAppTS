b75dea54603d00a8a0d1bac41c01ca7207d90b49
F4ELL <ferreiraperes1234@gmail.com>
2022-02-24 22:35:47 -0300

    fix: correction error scroll bar

--- src/components/ListTasks/StyledListTasks.ts
+++ src/components/ListTasks/StyledListTasks.ts
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 export const StyledListTasks = styled.ul`
     list-style: none;
     width: 600px;
-    height: 40vh;
-    overflow-y: scroll;
-    padding: 150px 10px 0 10px;
     display: flex;
     justify-content: center;
     flex-direction: column;
